41c555ff958c953b69a8b3fcdffe7f6f6edadbfc
tfjaeger <fjaeger@ur.rochester.edu>
2022-10-23 15:50:01 -0400

    moved 3d plotting functions into functions.R, moved constants, libraries, etc. into separate files.

---
+++ scripts/constants.R
@@ -0,0 +1,13 @@
+colors.voicing = c("blue", "red")
+
+# For plotting
+myGplot.defaults("paper")
+
+options(width = 200, digits = 2, OutDec = ".", tinytex.verbose = TRUE)
+base.width = 2.15
+base.height = 2.15
+
+animate = T         # switch on/off to replace animations with facets (not yet implemented for all instances)
+TODO = T            # switch on/off to get to do list.
+RESET_MODELS = T    # switch on/off whether models/optimization that exist as files are regenerated
+RESET_FIGURES = T   # switch on/off whether figures that exist as files are regenerated

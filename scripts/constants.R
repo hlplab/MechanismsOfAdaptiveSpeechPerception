edb8a6fd5d17a20bb92b2778dda71281ba551be0
tfjaeger <fjaeger@ur.rochester.edu>
2022-12-03 13:33:10 -0500

    updated preamble; moved constants relevant to knitting back into the main doc.

--- scripts/constants.R
+++ scripts/constants.R
@@ -6,8 +6,3 @@ myGplot.defaults("paper")
 options(width = 200, digits = 2, OutDec = ".", tinytex.verbose = TRUE)
 base.width = 2.15
 base.height = 2.15
-
-animate = T         # switch on/off to replace animations with facets (not yet implemented for all instances)
-TODO = T            # switch on/off to get to do list.
-RESET_MODELS = F    # switch on/off whether models/optimization that exist as files are regenerated
-RESET_FIGURES = T   # switch on/off whether figures that exist as files are regenerated
